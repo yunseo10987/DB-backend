5eacf06109e6368519d02659d41e74b17840f74b
yunseo <yunseo10987@naver.com>
2025-05-15 19:46:25 +0900

    FEAT : 기본 환경 설정

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require("express");
 require("dotenv").config();
 
+const errorHandler = require("./src/middlewares/errorHandler");
+const notFoundApi = require("./src/middlewares/notFoundApi");
+
 const postsApi = require("./src/routes/posts");
 
 const app = express();
 const port = process.env.HTTP_PORT;
 
 app.use(express.json());
+app.use("/users", usersApi);
 app.use("/posts", postsApi);
+
+app.use(notFoundApi);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`${port}번에서 HTTP Web Server 실행`);
