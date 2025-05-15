18dd391aee18e29dbf692a03c73af3409a8a8061
yunseo <yunseo10987@naver.com>
2025-05-15 19:44:58 +0900

    FEAT : 사용자 인증 기능 추가

--- src/midlewares/checkAuth.js
+++ src/midlewares/checkAuth.js
@@ -1,1 +1,25 @@
+const jwt = require("jsonwebtoken");
+
+const {
+    UnauthorizedException
+} = require("../model/customException");
+
+const checkAuth = (type = null) => {
+    return (req, res, next) => {
+        const authHeader = req.headers['authorization'];
+        const token = authHeader && authHeader.split(' ')[1]; // "Bearer <토큰>"에서 "<토큰>" 부분만 추출
+
+        try {
+            const jwtData = jwt.verify(token, process.env.TOKEN_SECRET_KEY);
+            req.decoded = jwtData;
+            
+            return next();
+        } catch (err) {
+            return next(new UnauthorizedException());
+        }
+    }
+}
+
+
+
 module.exports = checkAuth
