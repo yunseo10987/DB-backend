397da63418a9f7a0808683bd7d01f4ff444a843d
yunseo <yunseo10987@naver.com>
2025-05-16 15:29:20 +0900

    FEAT : 비로그인 옵션 추가

--- src/midlewares/checkAuth.js
+++ src/midlewares/checkAuth.js
@@ -4,12 +4,21 @@ const {
     UnauthorizedException
 } = require("../model/customException");
 
-const checkAuth = (type = null) => {
+const checkAuth = ({required = true} = {}) => {
     return (req, res, next) => {
         const authHeader = req.headers['authorization'];
         const token = authHeader && authHeader.split(' ')[1]; // "Bearer <토큰>"에서 "<토큰>" 부분만 추출
 
         try {
+            if (!token) {
+                if (required) {
+                    return next(new UnauthorizedException());
+                } else {
+                    req.decoded = null;
+                    return next();
+                }
+            }
+
             const jwtData = jwt.verify(token, process.env.TOKEN_SECRET_KEY);
             req.decoded = jwtData;
             
