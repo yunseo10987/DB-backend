548d4627b9bc2b1d00970eefc3c3c6c10cf129fa
dlrlans <nhtgb021030@gmail.com>
2025-05-18 00:52:58 +0900

    feat: 회원가입 로그인 추가

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const notFoundApi = require("./src/midlewares/notFoundApi");
 
 const postsApi = require("./src/routes/posts");
 const emotionsApi = require("./src/routes/emotions");
+const usersApi = require("./src/routes/users");
 
 const app = express();
 const port = process.env.HTTP_PORT;
@@ -13,6 +14,7 @@ const port = process.env.HTTP_PORT;
 app.use(express.json());
 app.use("/posts", postsApi);
 app.use("/emotions", emotionsApi);
+app.use("/users", usersApi);
 
 app.use(notFoundApi);
 app.use(errorHandler);
